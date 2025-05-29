08dd91604ebaecf872586d7f5ae9ed50fdbe2eec
Yusuf Kurniawan <89442208+ZyDorgGH@users.noreply.github.com>
2025-05-29 22:56:44 +0700

    Update data_split.py use Stratified Split method

--- dataset/data_split.py
+++ dataset/data_split.py
@@ -1,76 +1,101 @@
-# from https://github.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/blob/master/util_scripts/train_val_test_split.py
+# Split dataset with Stratified Split method
 
 from pathlib import Path
 import random
 import os
 import sys
+import xml.etree.ElementTree as ET
+from sklearn.model_selection import train_test_split
 
-# Set the paths for the image directories
+# Set the paths
 image_path = '/content/dataset/all'
 train_path = '/content/dataset/train'
 val_path = '/content/dataset/validation'
 test_path = '/content/dataset/test'
+labelmap_path = '/content/labelmap.txt'  # Path ke file labelmap
 
-# Gather all image files with different extensions from the source folder
-jpeg_file_list = [path for path in Path(image_path).rglob('*.jpeg')]
-jpg_file_list = [path for path in Path(image_path).rglob('*.jpg')]
-png_file_list = [path for path in Path(image_path).rglob('*.png')]
-bmp_file_list = [path for path in Path(image_path).rglob('*.bmp')]
+# Baca daftar kelas dari labelmap.txt
+with open(labelmap_path, 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
 
-# On Linux systems, also include uppercase file extensions (case sensitive)
+# Kumpulkan semua file gambar (sama seperti sebelumnya)
 if sys.platform == 'linux':
-    JPEG_file_list = [path for path in Path(image_path).rglob('*.JPEG')]
-    JPG_file_list = [path for path in Path(image_path).rglob('*.JPG')]
-    file_list = jpg_file_list + JPG_file_list + png_file_list + bmp_file_list + JPEG_file_list + jpeg_file_list
+    file_list = list(Path(image_path).rglob('*.[jJ][pP][gG]')) + \
+               list(Path(image_path).rglob('*.[jJ][pP][eE][gG]')) + \
+               list(Path(image_path).rglob('*.png')) + \
+               list(Path(image_path).rglob('*.bmp'))
 else:
-    file_list = jpg_file_list + png_file_list + bmp_file_list + jpeg_file_list
+    file_list = list(Path(image_path).rglob('*.jpg')) + \
+               list(Path(image_path).rglob('*.jpeg')) + \
+               list(Path(image_path).rglob('*.png')) + \
+               list(Path(image_path).rglob('*.bmp'))
 
-# Count the total number of image files found
-file_num = len(file_list)
-print('Total images: %d' % file_num)
+# 1. Ekstrak label utama untuk setiap gambar
+image_class_mapping = {}
 
-# Define the dataset split ratios: 70% for training, 15% for validation, and 15% for testing
-train_percent = 0.7
-val_percent = 0.15
-test_percent = 0.15
+for img_path in file_list:
+    xml_path = img_path.with_suffix('.xml')
+    if not xml_path.exists():
+        continue
 
-# Calculate the exact number of images for each subset
-train_num = int(file_num * train_percent)
-val_num = int(file_num * val_percent)
-test_num = file_num - train_num - val_num
-print('Images moving to train: %d' % train_num)
-print('Images moving to validation: %d' % val_num)
-print('Images moving to test: %d' % test_num)
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    
+    # Cari semua kelas dalam gambar
+    found_classes = set()
+    for obj in root.findall('object'):
+        class_name = obj.find('name').text
+        if class_name in classes:
+            found_classes.add(class_name)
+    
+    # Jika ada kelas yang valid, gunakan kelas pertama sebagai representasi
+    if found_classes:
+        image_class_mapping[img_path] = next(iter(found_classes))
 
-# Randomly select and move 70% of the images along with their annotation files to the training folder
-for i in range(train_num):
-    move_me = random.choice(file_list)
-    fn = move_me.name
-    base_fn = move_me.stem
-    parent_path = move_me.parent
-    xml_fn = base_fn + '.xml'
-    os.rename(move_me, os.path.join(train_path, fn))
-    os.rename(os.path.join(parent_path, xml_fn), os.path.join(train_path, xml_fn))
-    file_list.remove(move_me)
+# 2. Kelompokkan gambar berdasarkan kelas
+class_images = {cls: [] for cls in classes}
+for img_path, cls in image_class_mapping.items():
+    class_images[cls].append(img_path)
 
-# Randomly select and move 15% of the remaining images with annotations to the validation folder
-for i in range(val_num):
-    move_me = random.choice(file_list)
-    fn = move_me.name
-    base_fn = move_me.stem
-    parent_path = move_me.parent
-    xml_fn = base_fn + '.xml'
-    os.rename(move_me, os.path.join(val_path, fn))
-    os.rename(os.path.join(parent_path, xml_fn), os.path.join(val_path, xml_fn))
-    file_list.remove(move_me)
+# 3. Bagi setiap kelas secara stratified
+train_set = []
+val_set = []
+test_set = []
 
-# Move all remaining images and their annotation files to the testing folder
-for i in range(test_num):
-    move_me = random.choice(file_list)
-    fn = move_me.name
-    base_fn = move_me.stem
-    parent_path = move_me.parent
-    xml_fn = base_fn + '.xml'
-    os.rename(move_me, os.path.join(test_path, fn))
-    os.rename(os.path.join(parent_path, xml_fn), os.path.join(test_path, xml_fn))
-    file_list.remove(move_me)
+for cls, images in class_images.items():
+    if len(images) < 2:  # Skip kelas dengan <2 sampel
+        continue
+    
+    # Bagi 70% train, 30% sementara
+    train_temp, temp = train_test_split(images, test_size=0.3, random_state=42)
+    # Bagi 30% menjadi val dan test (50-50)
+    val, test = train_test_split(temp, test_size=0.5, random_state=42)
+    
+    train_set.extend(train_temp)
+    val_set.extend(val)
+    test_set.extend(test)
+
+print(f'Total train: {len(train_set)}')
+print(f'Total val: {len(val_set)}')
+print(f'Total test: {len(test_set)}')
+
+# 4. Pindahkan file ke folder tujuan
+def move_files(file_list, dest_dir):
+    for img_path in file_list:
+        xml_path = img_path.with_suffix('.xml')
+        if not xml_path.exists():
+            continue
+            
+        # Pindahkan gambar
+        os.rename(img_path, os.path.join(dest_dir, img_path.name))
+        # Pindahkan XML
+        os.rename(xml_path, os.path.join(dest_dir, xml_path.name))
+
+# Pastikan folder tujuan ada
+os.makedirs(train_path, exist_ok=True)
+os.makedirs(val_path, exist_ok=True)
+os.makedirs(test_path, exist_ok=True)
+
+move_files(train_set, train_path)
+move_files(val_set, val_path)
+move_files(test_set, test_path)
